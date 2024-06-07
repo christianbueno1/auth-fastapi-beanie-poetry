71b4c2f0741b3087e5fdb321c0833ae8a80b957d
Christian Bueno <chmabuen@espol.edu.ec>
2024-06-07 14:30:21 -0500

    refactor: move functions from crud to services
    to organize the project, move get_current_user get_current_active_user functions from crud/user.py to services/auth_services.py

--- auth_fastapi_beanie_poetry/crud/user.py
+++ auth_fastapi_beanie_poetry/crud/user.py
@@ -1,15 +1,4 @@
-from typing import Annotated
-from beanie import PydanticObjectId
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-import jwt
-from auth_fastapi_beanie_poetry.models.token import TokenData
 from auth_fastapi_beanie_poetry.models.user import User
-from auth_fastapi_beanie_poetry.core.config import core_settings
-from jwt.exceptions import InvalidTokenError
-from auth_fastapi_beanie_poetry.schemas.user import UserInDB
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def get_user(username: str) -> User | None:
     # user = await User.find_one({"username": username})
@@ -22,35 +11,5 @@ async def get_user_by_email(email: str) -> User | None:
     user_in_db = User(**user.model_dump())
     return user_in_db
 
-# create_user
 
-# get_current_user get_current_active_user
 
-# get_current_user
-# username is the sub claim of the JWT token
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, core_settings.SECRET_KEY, algorithms=[core_settings.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user: User = get_user(username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-# get_current_active_user
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
--- auth_fastapi_beanie_poetry/services/auth_services.py
+++ auth_fastapi_beanie_poetry/services/auth_services.py
@@ -1,7 +1,16 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
 from auth_fastapi_beanie_poetry.core.security import verify_password
 from auth_fastapi_beanie_poetry.crud.user import get_user
+from auth_fastapi_beanie_poetry.models.token import TokenData
 from auth_fastapi_beanie_poetry.models.user import User
+from auth_fastapi_beanie_poetry.core.config import core_settings
+from jwt.exceptions import InvalidTokenError
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def authenticate_user(username: str, password: str) -> User | False:
     user: User = await get_user(username)
@@ -13,3 +22,32 @@ async def authenticate_user(username: str, password: str) -> User | False:
     return user
 
 # get_current_user get_current_active_user
+
+# get_current_user
+# username is the sub claim of the JWT token
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, core_settings.SECRET_KEY, algorithms=[core_settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user: User = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+# get_current_active_user
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
